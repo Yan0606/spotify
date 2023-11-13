3389b9aed5f3a1483d06b7c34012a6c77fc41e5a
yan0606 <yan060604@gmail.com>
2023-11-12 23:17:12 -0300

    creating the login CSS

--- hamburgueria-celso/resources/views/home.blade.php
+++ hamburgueria-celso/resources/views/home.blade.php
@@ -4,17 +4,27 @@
 
 @section('content')
 <link rel="stylesheet" href="/public/css/style.css">
+<header class="spotify-header">
+    <img src="/img/logo.png" alt="Logo Spotify">
+  </header>
 
-<h1>CADSTRO</h1>
 <form method="POST" action="/">
     @csrf 
+    <h1>Entrar no Spotify</h1>
+    <div class="cadasto">
+    <input type="button" value="Continuar com Google" id="cadastro" class="google-button">
+    <input type="button" value="Continuar com Facebook" id="cadastro" class="face-button">
+    <input type="button" value="Continuar com Apple" id="cadastro" class="apple-button">
+    <input type="button" value="Continuar com um numero de Telefone" id="cadastro">
+    </div>
     <label>Nome: </label>
     <input type="text" name="name" placeholder="Digite seu nome">
     <label>E-mal: </label>
     <input type="text" name="email" placeholder="Digite seu e-mail">
     <label>Password: </label>
     <input type="password" name="password" placeholder="Digite sua senha">
-    <input type="submit" name="Cadastrar" value="Cadastrar">
+    <input type="submit" name="Cadastrar" value="Entrar">
+    
 </form>
 
-@endsection
+<!-- @endsection 
